908b3315493c1b53498c601d7ead5ec00b57212f
Adrian González Pardo <gozapaadr@gmail.com>
2019-09-03 12:53:42 -0500

    Update installJava.sh

--- installJava.sh
+++ installJava.sh
@@ -3,4 +3,4 @@
 sudo add-apt-repository ppa:linuxuprising/java --yes
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EA8CACC073C3DB2A
 sudo apt update
-sudo apt install oracle-java11-installer -y
+sudo apt install oracle-java12-installer -y
