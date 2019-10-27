c1bdf8c4dc277d1721cf518fae72323061221d0a
Adrian González Pardo <gozapaadr@gmail.com>
2019-10-27 14:21:24 -0600

    Modify the version jdk12 to jdk13

--- installJava.sh
+++ installJava.sh
@@ -3,5 +3,5 @@
 sudo add-apt-repository ppa:linuxuprising/java --yes
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EA8CACC073C3DB2A
 sudo apt update
-sudo apt install oracle-java12-installer -y
-sudo apt install oracle-java12-set-default -y
+sudo apt install oracle-java13-installer -y
+sudo apt install oracle-java13-set-default -y
